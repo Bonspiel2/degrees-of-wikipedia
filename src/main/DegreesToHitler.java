18ecbff653c292cf69b3308c2120d32d1272047e
Bonspiel2 <cole_adams@shaw.ca>
2015-12-09 15:39:59 -0700

    Added everything

--- src/main/DegreesToHitler.java
+++ src/main/DegreesToHitler.java
@@ -1,15 +1,21 @@
 package main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.security.auth.login.FailedLoginException;
 
 import org.wikipedia.Wiki;
 
 public class DegreesToHitler {
+	
+	private static String path = "";
+	
+	private static String alreadyVisited = "";
+	static Wiki wiki = new Wiki("en.wikipedia.org");
 
 	public static void main(String[] args) throws IOException {
-		Wiki wiki = new Wiki("en.wikipedia.org");
+		Scanner in = new Scanner(System.in);
 		
 		wiki.setThrottle(0);
 	
@@ -20,15 +26,64 @@ public class DegreesToHitler {
 			e.printStackTrace();
 		}
 		
-		String[] links = wiki.getLinksOnPage("Adolf Hitler");
+		System.out.println("Enter a page to start from: ");
+		String firstPage = in.nextLine();
 		
-		for (String link : links){
-			System.out.println(link);
-		}
+		String[] links = wiki.getLinksOnPage(firstPage);
+		
+		path = firstPage + " >";
+		
+		findHitler(firstPage, 5);
+		
+		System.out.println(path);
+		
+//		for (String link : links){
+//			System.out.println(link);
+//		}
+		
 		
 		wiki.logout();
 		
 
 	}
+	
+	public static boolean findHitler(String page, int degrees) throws IOException{
+		if (degrees == 0){
+			return false;
+		}
+		
+		String[] links = wiki.getLinksOnPage(page);
+	
+		
+		if (contains(links, "Adolf Hitler")){
+			path+= " Adolf Hitler";
+			return true;
+		} else {
+			
+			for (int i = 0; i < links.length; i++){
+				if (!alreadyVisited.contains("~" + links[i] + "~")){
+					if (findHitler(links[i], degrees - 1)){
+						path = page + " >" + path;
+						return true;
+					} else {
+						alreadyVisited+="~" + links[i] + "~";
+					}
+				}
+			}
+		}
+		return false;
+		
+	}
+	
+	private static boolean contains(String[] links, String page){
+		for (String link : links){
+			if (link.equals(page)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 
 }
