dce4d06bdd7bc548de98c98d5796349676c87884
Bonspiel2 <cafrench99@gmail.com>
2015-12-09 23:47:01 -0700

    Added checking for the articles that link to hitler

--- src/main/DegreesToHitler.java
+++ src/main/DegreesToHitler.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import javax.security.auth.login.FailedLoginException;
@@ -13,6 +14,8 @@ public class DegreesToHitler {
 	
 	private static String alreadyVisited = "";
 	static Wiki wiki = new Wiki("en.wikipedia.org");
+	
+	private static String[] linksToHitler;
 
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
@@ -26,20 +29,22 @@ public class DegreesToHitler {
 			e.printStackTrace();
 		}
 		
+		linksToHitler = wiki.whatLinksHere("Adolf Hitler");
+		
 		System.out.println("Enter a page to start from: ");
 		String firstPage = in.nextLine();
 		
 		String[] links = wiki.getLinksOnPage(firstPage);
 		
-		path = firstPage + " >";
+		links = clean(links);
 		
-		findHitler(firstPage, 5);
+		//findHitler(firstPage, 5);
 		
-		System.out.println(path);
+		//System.out.println(path);
 		
-//		for (String link : links){
-//			System.out.println(link);
-//		}
+		for (String link : links){
+			System.out.println(link);
+		}
 		
 		
 		wiki.logout();
@@ -53,13 +58,22 @@ public class DegreesToHitler {
 		}
 		
 		String[] links = wiki.getLinksOnPage(page);
+		
+		links = clean(links);
 	
 		
 		if (contains(links, "Adolf Hitler")){
-			path+= " Adolf Hitler";
+			path+= page + " > Adolf Hitler";
 			return true;
+		} else if (degrees == 1){
+			return false;
 		} else {
+			String linkToClick = closeToHitler(links);
 			
+			if (!linkToClick.isEmpty()){
+				path+= page + " >" + linkToClick + " > Adolf Hitler";
+				return true;
+			}
 			for (int i = 0; i < links.length; i++){
 				if (!alreadyVisited.contains("~" + links[i] + "~")){
 					if (findHitler(links[i], degrees - 1)){
@@ -83,6 +97,27 @@ public class DegreesToHitler {
 		}
 		return false;
 	}
+	
+	private static String closeToHitler(String[] links){
+		for (int i = 0; i < links.length; i++){
+			if (contains(linksToHitler, links[i])){
+				return links[i];
+			}
+		}
+		return "";
+	}
+	
+	private static String[] clean(String[] links){
+		
+		for (int i = 0; i < links.length; i++){
+			if (links[i].contains("Wikipedia:")){
+				links = Arrays.copyOf(links, i);
+			}
+		}
+		return links;
+		
+	}
+	
 	
 	
 
